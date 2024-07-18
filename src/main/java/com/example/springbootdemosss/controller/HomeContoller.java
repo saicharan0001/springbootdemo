fbbec669fb94af0dda41e5b08a9c78fc021d2727
saicharan <saicharan_b200970ec@nitc.ac.in>
2024-07-18 17:09:43 +0530

    some commit

--- src/main/java/com/example/springbootdemosss/controller/HomeContoller.java
+++ src/main/java/com/example/springbootdemosss/controller/HomeContoller.java
@@ -1,12 +1,27 @@
 package com.example.springbootdemosss.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springbootdemosss.model.User;
 
 @RestController
 public class HomeContoller {
  @GetMapping("/hello")
  public String greet() {
 	 return "welcome";
+ }
+ @GetMapping("/users")
+ public List users() {
+	 List<User> u = new ArrayList<User>();
+	 u.add(new User(1,"sai","head"));
+	 u.add(new User(2,"charan","head"));
+	 u.add(new User(3,"abhi","head"));
+	 u.add(new User(4,"hello","head"));
+	 u.add(new User(5,"sai","head"));
+	 return u;
  }
 }
---
+++ src/main/java/com/example/springbootdemosss/model/User.java
@@ -0,0 +1,12 @@
+package com.example.springbootdemosss.model;
+
+public class User {
+   public int id;
+   public String name;
+   public String design;
+   public User(int id,String name,String design){
+	   this.id = id;
+	   this.name = name;
+	   this.design = design;
+   }
+}
